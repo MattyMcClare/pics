2f26ceb9eb1139e16942cbb4c190b5798eed871b
Mateusz <matstanczak@gmail.com>
2020-02-22 16:17:25 +0000

    ImageList refactor and added alt props to the image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    const images = props.images.map((image) => {
-        return <img src={image.urls.regular} />
+    console.log(props.images)
+    const images = props.images.map(({alt_description, id, urls}) => {
+        return <img key={id} alt={alt_description} src={urls.regular} />;
     });
     return(
         <div>{images}</div>
     )
-}
+};
 
 export default ImageList;
